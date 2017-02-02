63237b85c324659d68f13e8b518ea0a45fba9611
eboni <eboni.jackson14@gmail.com>
2017-02-02 11:36:57 -0500

    butterfly

--- Week2/Week 2 Notes/sketch.js
+++ Week2/Week 2 Notes/sketch.js
@@ -1,7 +1,60 @@
-function setup() {
+/*
+var num = 100;
+var num2 = 50;
+var trex = 1000;
+var zeven = 7;
+var message ="hello";
+var anotherMessage = "goodbye";
+var booleanVar = true;
+var booleanVar2 = false;
 
+function setup() {
+console.log(num);
+console.log("num + num2=" + (num + num2));
+console.log(num * num2);
+console.log(num/num2);
+console.log(message + anotherMessage);
+console.log(message + " " + anotherMessage)
+console.log(booleanVar);
+console.log(message + trex);
+    //This is not going to work
+console.log(message * trex);
+    
+If you want to do more stuff
+this is a multi line comment
+
+
+var message2 = "This only works in the setup";
+console.log(message2);
+    
+    
 }
 
 function draw() {
-  
+  console.log(num);
+    var message3 ="This only works in the draw";
+}
+*/
+var mouthX = 25;
+var mouthY = 250;
+var mouthW = 400;
+var mouthH = 60
+
+function setup () {
+    createCanvas(600,400);
+    background(255,255,0);
+    
+    console.log("width:" + width + " " + "height:" + height)
+    
+}
+
+function draw () {
+    // x, y, width, height
+    ellipse(70,70,100,100);
+    
+    ellipse(width/2,height/2,50,50);
+    
+    rect(mouthX,mouthY,mouthW,mouthH);
+    line(mouthX, mouthY + mouthH/2, mouthX + mouthW, mouthY + mouthH/2);
+    
 }
---
+++ Week2/week2assignment/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+createCanvas (600,600);
+background (244,188,222);
+
+console.log("width:" + width + " " + "height" + height)
+}
+
+function draw() {
+  ellipse(300,100,50,50)
+  ellipse(300,275,100,300)
+  ellipse(405,200,125,100)
+  ellipse(440,325,200,150)
+  ellipse(195,200,125,100)
+  ellipse(160,325,200,150)
+  
+ 
+  
+}
